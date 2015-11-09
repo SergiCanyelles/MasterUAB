a1ba29e5b22971f1b0e26c9e39c01717eb2a8a69
SergiCanyelles <scanyelles@gmail.com>
2015-11-09 21:18:03 +0100

    no message

--- MasterUAB/VideogameTest/videogame.cpp
+++ MasterUAB/VideogameTest/videogame.cpp
@@ -1,7 +1,16 @@
 #include <Windows.h>
+#include <d3d11.h>
 
-#define APPLICATION_NAME	"VIDEOGAME"
+#pragma comment(lib,"d3d11.lib")
 
+#define APPLICATION_NAME	"VIDEOGAMETEST"
+
+	ID3D11Device *l_D3DDevice; // esta clase, el device, nos sirve para crear objetos de DirectX
+	ID3D11DeviceContext *l_DeviceContext; // el contexto nos va a servir para usar objetos de DirectX
+	IDXGISwapChain *l_SwapChain; // la cadena de swap
+	int WIDTH_APPLICATION = 800;
+	int HEIGHT_APPLICATION = 600;
+	LRESULT CreateContext(HWND &hWnd, int width, int height);
 
 //-----------------------------------------------------------------------------
 // Name: MsgProc()
@@ -40,19 +49,55 @@ LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
 //-----------------------------------------------------------------------
 int APIENTRY WinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _lpCmdLine, int _nCmdShow)
 {
+	int width;
+	int height;
+
   // Register the window class
   WNDCLASSEX wc = {	sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, APPLICATION_NAME, NULL };
 
   RegisterClassEx( &wc );
 
+  	l_D3DDevice = 0;
+	l_DeviceContext = 0;
+	l_SwapChain = 0;
+
+  /*
   // Create the application's window
   HWND hWnd = CreateWindow(	APPLICATION_NAME, APPLICATION_NAME, WS_OVERLAPPEDWINDOW, 100, 100, 800, 600, NULL, NULL, wc.hInstance, NULL );
 
 
+
   // Añadir aquí el Init de la applicacioón
 
+  
   ShowWindow( hWnd, SW_SHOWDEFAULT );
   UpdateWindow( hWnd );
+
+  */
+  	// Calcular el tamano de nuestra ventana
+	RECT rc = {
+			0, 0, WIDTH_APPLICATION, HEIGHT_APPLICATION
+		};
+	AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE);
+	
+	width =  rc.right - rc.left;
+	height =  rc.bottom - rc.top;
+
+	// Crear la ventana en si
+	HWND hWnd = CreateWindow( APPLICATION_NAME, APPLICATION_NAME, WS_OVERLAPPEDWINDOW, 100, 100, width, height, NULL, NULL, wc.hInstance, NULL);
+
+	// TODO Crear el contexto DIRECTX
+	CreateContext(hWnd, width, height);
+
+	// Mostrar la ventana
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+
+	// TODO Crear el back buffer
+	//CreateBackBuffer( hWnd, WIDTH_APPLICATION, HEIGHT_APPLICATION );
+
+	UpdateWindow(hWnd);
+
+
   MSG msg;
   ZeroMemory( &msg, sizeof(msg) );
 
@@ -75,4 +120,52 @@ int APIENTRY WinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _lpCm
   // Añadir una llamada a la alicación para finalizar/liberar memoria de todos sus datos
 
   return 0;
+}
+
+/***************************************************************************************
+**  Para pintar en DirectX, primero necesitamos crear un contexto de DirectX.         **
+**  Para ello vamos a definir la "cadena de intercambio", o sea, cómo se va a         **
+**  comportar nuestro programa cada "frame"                                           **
+***************************************************************************************/
+LRESULT CreateContext(HWND &hWnd, int width, int height)
+{
+	// Tendremos que crear y rellenar una estructura de este tipo
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	// o
+	//DXGI_SWAP_CHAIN_DESC desc = {};
+	desc.BufferCount = 1;
+	desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.Windowed = TRUE;
+	desc.BufferDesc.Width = width;
+	desc.BufferDesc.Height = height;
+	desc.BufferDesc.RefreshRate.Numerator = 60;
+	desc.BufferDesc.RefreshRate.Denominator = 1;
+	desc.OutputWindow = hWnd;
+	desc.SampleDesc.Count = 1;
+	//desc.SampleDesc.Quality;
+	//desc. ????
+
+	// Que DirectX queremos
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_10_1,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	ID3D11Device *l_D3DDevice; // esta clase, el device, nos sirve para crear objetos de DirectX
+	ID3D11DeviceContext *l_DeviceContext; // el contexto nos va a servir para usar objetos de DirectX
+	IDXGISwapChain *l_SwapChain; // la cadena de swap
+
+	HRESULT ret = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, featureLevels, numFeatureLevels, D3D11_SDK_VERSION, &desc, &l_SwapChain, &l_D3DDevice, NULL, &l_DeviceContext);
+
+	if (FAILED(ret))
+	{
+		return S_FALSE;
+	}else{
+		return 1;
+	}
 }
